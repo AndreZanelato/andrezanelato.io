2b47d4b6b5c2b67c6e1da4068ec6de437b626546
AndreZanelato <andre.assis.zanelato@gmail.com>
2023-09-28 20:20:41 -0300

    Site update

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const user = 'andrezanelato';
+const pass = '1597'
+
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('login-form');
+
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        // Aqui você pode adicionar lógica de autenticação
+        // Verifique se o nome de usuário e senha correspondem
+        if (username === user && password === pass) {
+            // Redirecione para a página de notas se o login for bem-sucedido
+            window.location.href = 'notes.html';
+        } else {
+            // Exiba uma mensagem de erro se o login falhar
+            alert('Nome de usuário ou senha incorretos. Tente novamente.');
+        }
+    })
+})
+
 document, addEventListener('DOMContentLoaded', function () {
     const noteTextarea = this.document.getElementById('note');
 
